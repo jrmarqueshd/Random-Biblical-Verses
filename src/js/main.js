4c9053234ba4220c4c6ad66ec7f1d7f098990d73
jrmarqueshd <gilmar_junior@hotmail.com.br>
2019-08-24 16:33:43 -0300

    create interval to show field share

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,16 @@ window.addEventListener("load", () => {
     let $bookContent = document.getElementById("bookContent");
     let $capVerContent = document.getElementById("capVerContent");
 
-    function GenerateRandomVerses(lastBook) {
+    let $fieldShare = document.getElementById("share");
+
+    function generateRandomVerses(lastBook) {
         return Math.floor(Math.random() * (lastBook - 0 + 1) - 0 );
+    }
+
+    function showFieldShare(interval){
+        setInterval(() => {
+            $fieldShare.classList.add("on");
+        }, interval);
     }
 
     fetch("../assets/data/bible.json")
@@ -14,22 +22,23 @@ window.addEventListener("load", () => {
         })
         .then((bible) => {
             this.allBooks = (bible.length - 1);
-            let getRandomBook = GenerateRandomVerses(allBooks);
+            let getRandomBook = generateRandomVerses(allBooks);
             const allChapters = (bible[getRandomBook].chapters.length - 1);
-            let getRandomChapter = GenerateRandomVerses(allChapters);
+            let getRandomChapter = generateRandomVerses(allChapters);
             const allVerses = (bible[getRandomBook].chapters[getRandomChapter].length - 1);
-            let getRandomVerse = GenerateRandomVerses(allVerses);
+            let getRandomVerse = generateRandomVerses(allVerses);
             let abbrev = bible[getRandomBook].abbrev;
             let bookName = bible[getRandomBook].name;
             let chapter = getRandomChapter + 1;
             let verse = getRandomVerse + 1;
             let verseContent = bible[getRandomBook].chapters[getRandomChapter][getRandomVerse];
 
-
             $abbrevBookContent.innerText = abbrev;
             $bookContent.innerText = bookName;
             $verseContent.innerText = verseContent;
             $capVerContent.innerText = `${chapter}:${verse}`;
+            
+            showFieldShare(10000);
         })
         .catch((err) => {
             console.log(err);
